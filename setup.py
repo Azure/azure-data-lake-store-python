3d30e2b2e1d4f4c0fa27b27dd5aaaf85030988d7
Martin Durant <martin.durant@utoronto.ca>
2016-07-24 17:32:04 -0400

    Setup basic package structure

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+setup(name='adlfs',
+      version='0.0.1',
+      description='Convenient Filesystem interface to Azure Data-lake Store',
+      url='https://github.com/Azure/azure-data-lake-store-python',
+      maintainer='',
+      maintainer_email='',
+      license='',
+      keywords='azure',
+      packages=['adlfs'],
+      install_requires=[open('requirements.txt').read().strip().split('\n')],
+      long_description=(open('README.rst').read() if os.path.exists('README.rst')
+                        else ''),
+      zip_safe=False)
