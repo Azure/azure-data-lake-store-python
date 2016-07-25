151cc8d358a4fb5c0a8557fb8570a3614f7c5570
Martin Durant <martin.durant@utoronto.ca>
2016-07-25 15:56:08 -0400

    Working directory downloader
    Notes added in the code.
    
    Will need to decide how to recreate locally the remote directory tree as returned
    by walk() - right now turns into flat file-list.

--- adlfs/multithread.py
+++ adlfs/multithread.py
@@ -22,7 +22,8 @@ def get_chunk(adlfs, rfile, lfile, offset, size):
 def threaded_file_downloader(adlfs, threadpool, rfile, lfile, chunksize):
     fsize = adlfs.info(rfile)['length']
     root = os.path.split(lfile)[0]
-    if not os.path.exists(root):
+    if not os.path.exists(root) and root:
+        # don't attempt to create current directory
         os.makedirs(root)
     offsets = range(0, fsize, chunksize)
     with open(lfile, 'wb'):
@@ -30,14 +31,19 @@ def threaded_file_downloader(adlfs, threadpool, rfile, lfile, chunksize):
         # TODO : log creating file
     futures = [threadpool.submit(get_chunk, adlfs, rfile, lfile, o, chunksize)
                for o in offsets]
+    # TODO : add 'done' callbacks and log
     return futures
 
 
 def download(adlfs, rpath, lpath, nthreads=None, chunksize=2**26):
     rfiles = adlfs.walk(rpath)
+    # TODO : wrap in a class, so can save and re-start failed/cancelled futures
     pool = ThreadPoolExecutor(max_workers=nthreads)
-    if len(flist) > 1:
-        lfiles = [os.path.join(root, os.path.split(f)[1]) for f in rfiles]
-        futures = sum(threaded_file_downloader(adlfs, pool, rfile, lfile,
-                                               chunksize), [])
-        return wait(futures)
+    if (len(rfiles) > 1) or (os.path.exists(lpath) and os.path.isdir(lpath)):
+        lfiles = [os.path.join(lpath, os.path.split(f)[1]) for f in rfiles]
+    else:
+        lfiles = [lpath]
+    # TODO : save in dict by parameters (or hash of)
+    futures = sum([threaded_file_downloader(adlfs, pool, rfile, lfile,
+                   chunksize) for (rfile, lfile) in zip(rfiles, lfiles)], [])
+    return wait(futures)
