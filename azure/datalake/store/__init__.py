1f6098a34798de50c42fa183f3fbe7d6340b8b2e
begoldsm <begoldsm@microsoft.com>
2017-01-18 16:42:41 -0800

    Revert version change.
    Will do this in a separate PR.

--- azure/datalake/store/__init__.py
+++ azure/datalake/store/__init__.py
@@ -6,7 +6,7 @@
 # license information.
 # --------------------------------------------------------------------------
 
-__version__ = "0.0.2"
+__version__ = "0.0.1"
 
 from .core import AzureDLFileSystem
 from .multithread import ADLDownloader
