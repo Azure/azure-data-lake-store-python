c07843c3f2e4cf31e759861403602a12983a657b
begoldsm <begoldsm@microsoft.com>
2017-02-03 11:56:21 -0800

    fix for folder overwrite.
    Merge from master to keep dev up to date.
    
    version bump.

--- azure/datalake/store/__init__.py
+++ azure/datalake/store/__init__.py
@@ -6,7 +6,7 @@
 # license information.
 # --------------------------------------------------------------------------
 
-__version__ = "0.0.2"
+__version__ = "0.0.4"
 
 from .core import AzureDLFileSystem
 from .multithread import ADLDownloader
