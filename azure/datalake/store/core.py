741dd61609b381b5b621d262b158f5c7bbf2bfaa
begoldsm <begoldsm@microsoft.com>
2017-04-25 18:07:39 -0700

    Introduce a small backoff retry as an experiment
    Version update and revision history.

--- azure/datalake/store/core.py
+++ azure/datalake/store/core.py
@@ -916,7 +916,7 @@ def _fetch_range(rest, path, start, end, stream=False):
 
 
 def _fetch_range_with_retry(rest, path, start, end, stream=False, retries=10,
-                            delay=0.01, backoff=1):
+                            delay=0.01, backoff=3):
     err = None
     for i in range(retries):
         try:
@@ -936,7 +936,7 @@ def _put_data(rest, op, path, data, **kwargs):
     return rest.call(op, path=path, data=data, **kwargs)
 
 
-def _put_data_with_retry(rest, op, path, data, retries=10, delay=0.01, backoff=1,
+def _put_data_with_retry(rest, op, path, data, retries=10, delay=0.01, backoff=3,
                          **kwargs):
     err = None
     for i in range(retries):
--- azure/datalake/store/multithread.py
+++ azure/datalake/store/multithread.py
@@ -246,7 +246,7 @@ class ADLDownloader(object):
 
 
 def get_chunk(adlfs, src, dst, offset, size, buffersize, blocksize,
-              shutdown_event=None, retries=10, delay=0.01, backoff=1):
+              shutdown_event=None, retries=10, delay=0.01, backoff=3):
     """ Download a piece of a remote file and write locally
 
     Internal function used by `download`.
