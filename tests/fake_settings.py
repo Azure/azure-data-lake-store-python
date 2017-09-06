61be2a509bfe684df0f873488585584b56f8b9c6
chdevala <chdevala@microsoft.com>
2017-09-06 11:50:45 -0700

    import os missing

--- tests/fake_settings.py
+++ tests/fake_settings.py
@@ -5,6 +5,7 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
+import os
 
 STORE_NAME = os.environ['azure_data_lake_store_name']
 TENANT_ID = os.environ['azure_tenant_id']
