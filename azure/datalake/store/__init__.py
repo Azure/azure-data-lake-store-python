d18e47dcbe4d1e9d7d6a9ecef72448abf6a5c325
chdevala <25624934+chdevala@users.noreply.github.com>
2017-09-21 17:53:29 -0700

    Update init.py to reflect new version
    releasing new version 0.0.17

--- azure/datalake/store/__init__.py
+++ azure/datalake/store/__init__.py
@@ -6,7 +6,7 @@
 # license information.
 # --------------------------------------------------------------------------
 
-__version__ = "0.0.16"
+__version__ = "0.0.17"
 
 from .core import AzureDLFileSystem
 from .multithread import ADLDownloader
