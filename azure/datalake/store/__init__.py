e30e9d48ebd798c68108fdef2845623076526ceb
Milan Chandna <milan.chandna@gmail.com>
2018-06-04 12:11:50 +0530

    updating versions

--- azure/datalake/store/__init__.py
+++ azure/datalake/store/__init__.py
@@ -6,7 +6,7 @@
 # license information.
 # --------------------------------------------------------------------------
 
-__version__ = "0.0.21"
+__version__ = "0.0.22"
 
 from .core import AzureDLFileSystem
 from .multithread import ADLDownloader
