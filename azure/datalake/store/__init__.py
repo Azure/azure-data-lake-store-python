4b203ee6647a686ac1969c68bf2a59b0db5f8595
Akshat Harit <38331238+akharit@users.noreply.github.com>
2018-10-15 17:47:42 -0700

    Updated version to 0.0.34. Pypi doesn't allow name reuse

--- azure/datalake/store/__init__.py
+++ azure/datalake/store/__init__.py
@@ -6,7 +6,7 @@
 # license information.
 # --------------------------------------------------------------------------
 
-__version__ = "0.0.33"
+__version__ = "0.0.34"
 
 from .core import AzureDLFileSystem
 from .multithread import ADLDownloader
