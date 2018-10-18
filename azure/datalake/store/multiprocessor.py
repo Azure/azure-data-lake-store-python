aa375f3686d1a0b552d2f2d05c08dcb88bbcf59e
Akshat Harit <akharit@microsoft.com>
2018-10-18 00:55:37 +0000

    Fix issue where process is hung due to uncaught exception

--- azure/datalake/store/multiprocessor.py
+++ azure/datalake/store/multiprocessor.py
@@ -5,6 +5,7 @@ import logging
 import multiprocessing
 import os
 import logging.handlers
+
 try:
     from queue import Empty     # Python 3
 except ImportError:
@@ -47,19 +48,27 @@ def multi_processor_change_acl(adl, path=None, method_name="", acl_spec=""):
         return process_list
 
     def walk(walk_path):
-        paths = []
-        all_files = adl.ls(path=walk_path, detail=True)
-        for files in all_files:
-            if files['type'] == 'DIRECTORY':
-                dir_processed_counter.increment()               # A new directory to process
-                walk_thread_pool.submit(walk, files['name'])
-            paths.append(files['name'])
-            if len(paths) == queue_bucket_size:
-                file_path_queue.put(list(paths))
-                paths = []
+        try:
+            paths = []
+            all_files = adl.ls(path=walk_path, detail=True)
 
-        file_path_queue.put(list(paths))  # For leftover paths < bucket_size
-        dir_processed_counter.decrement()         # Processing complete for this directory
+            for files in all_files:
+                if files['type'] == 'DIRECTORY':
+                    dir_processed_counter.increment()               # A new directory to process
+                    walk_thread_pool.submit(walk, files['name'])
+                paths.append(files['name'])
+                if len(paths) == queue_bucket_size:
+                    file_path_queue.put(list(paths))
+                    paths = []
+            file_path_queue.put(list(paths))  # For leftover paths < bucket_size
+        except:
+            import sys, traceback
+            logger.exception("Failed to walk for path: " + str(walk_path) + ". Exiting!")
+            for c in child_processes:
+                c.terminate()
+                c.join()
+            sys.exit(1)
+        dir_processed_counter.decrement()           # Processing complete for this directory
 
     finish_queue_processing_flag = multiprocessing.Event()
     file_path_queue = multiprocessing.JoinableQueue()
@@ -71,15 +80,18 @@ def multi_processor_change_acl(adl, path=None, method_name="", acl_spec=""):
     dir_processed_counter = CountUpDownLatch()
     walk_thread_pool = ThreadPoolExecutor(max_workers=worker_thread_num_per_process)
 
-    file_path_queue.put([path])  # Root directory to initialize walk
+    file_path_queue.put([path])  # Root directory needs to be passed
     dir_processed_counter.increment()
     walk(path)                  # Start processing root directory
 
-    if dir_processed_counter.is_zero():  # Done processing all directories. Blocking call.
+    if dir_processed_counter.is_zero(): # Done processing all directories. Blocking call.
+        walk_thread_pool.shutdown()
+        file_path_queue.close()          # No new elements to add
         file_path_queue.join()           # Wait for operations to be done
+        logger.log(logging.DEBUG, "file path queue closed")
         finish_queue_processing_flag.set()  # Set flag to break loop of child processes
         for child in child_processes:  # Wait for all child process to finish
-            logger.log(logging.DEBUG, "Thread pool for worked threads for walk shut down")
+            logger.log(logging.DEBUG, "Joining process: "+str(child.pid))
             child.join()
 
     # Cleanup
@@ -90,14 +102,10 @@ def multi_processor_change_acl(adl, path=None, method_name="", acl_spec=""):
     logger.log(logging.DEBUG, "Log queue closed")
     log_listener.join()
     logger.log(logging.DEBUG, "Log thread finished")
-    walk_thread_pool.shutdown()
-    logger.log(logging.DEBUG, "Thread pool for worked threads for walk shut down")
-    file_path_queue.close()
-    logger.log(logging.DEBUG, "File path queue closed")
+
 
 
 def processor(adl, file_path_queue, finish_queue_processing_flag, method_name, acl_spec, log_queue):
-
     logger = logging.getLogger(__name__)
 
     try:
@@ -111,7 +119,6 @@ def processor(adl, file_path_queue, finish_queue_processing_flag, method_name, a
     try:
         worker_thread_num_per_process = 50
         func_table = {"mod_acl": adl.modify_acl_entries, "set_acl": adl.set_acl, "rem_acl": adl.remove_acl_entries}
-        running_thread_count = CountUpDownLatch()
         function_thread_pool = ThreadPoolExecutor(max_workers=worker_thread_num_per_process)
         adl_function = func_table[method_name]
         logger.log(logging.DEBUG, "Started processor pid:"+str(os.getpid()))
@@ -121,25 +128,21 @@ def processor(adl, file_path_queue, finish_queue_processing_flag, method_name, a
                 func(path=path, acl_spec=spec)
             except:
                 pass    # Exception is being logged in the relevant acl method. Do nothing here
-            running_thread_count.decrement()
             logger.log(logging.DEBUG, "Completed running on path:" + str(path))
 
-        while not finish_queue_processing_flag.is_set():
+        while finish_queue_processing_flag.is_set() == False:
             try:
                 file_paths = file_path_queue.get(timeout=0.1)
-                file_path_queue.task_done()
+                file_path_queue.task_done()                 # Will not be called if empty
                 for file_path in file_paths:
-                    running_thread_count.increment()
                     logger.log(logging.DEBUG, "Starting on path:" + str(file_path))
                     function_thread_pool.submit(func_wrapper, adl_function, file_path, acl_spec)
             except Empty:
                 pass
 
-        if running_thread_count.is_zero():  # Blocking call. Will wait till all threads are finished.
-            pass
-        function_thread_pool.shutdown()
     except Exception as e:
+
         logger.exception("Exception in pid "+str(os.getpid())+"Exception: " + str(e))
     finally:
-        function_thread_pool.shutdown()
+        function_thread_pool.shutdown() # Blocking call. Will wait till all threads are done executing.
         logger.log(logging.DEBUG, "Finished processor pid: " + str(os.getpid()))
