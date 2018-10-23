54927ef2f4dada53d9f14125127cf0d70fe16e8e
Akshat Harit <38331238+akharit@users.noreply.github.com>
2018-10-23 12:36:34 -0700

    Added tests

--- tests/test_mock.py
+++ tests/test_mock.py
@@ -17,10 +17,12 @@ This was introduced to test the Retry Policy but can be carefully used for other
 """
 
 import responses
-from requests import ConnectionError, ConnectTimeout, ReadTimeout, Timeout
+from requests import ConnectionError, ConnectTimeout, ReadTimeout, Timeout, HTTPError
 
 from tests import settings
 from azure.datalake.store.exceptions import DatalakeRESTException
+from azure.datalake.store.lib import auth
+from azure.datalake.store.lib import DataLakeCredential
 from tests.testing import azure, azure_teardown, posix, working_dir
 test_dir = working_dir()
 
@@ -91,3 +93,46 @@ def __test_retry_error(azure,
         assert not is_exception_expected
     except DatalakeRESTException:
         assert is_exception_expected
+
+
+@responses.activate
+def __test_retry_auth(error_code, error_string, is_exception_expected, total_tries=4, last_try_status=200,
+                      last_try_body=r'{"token_type":"Bearer","expires_in":"1","ext_expires_in":"1","expires_on":"1","not_before":"1","resource":"https://datalake.azure.net/","access_token":"a"}'):
+    import re, adal
+    end_point = re.compile("https:\/\/login\.windows\.net\/common\/discovery\/instance\?authorization_endpoint=.+")
+    mock_url = "https://login.windows-ppe.net/" + settings.TENANT_ID+ "/oauth2/token"
+
+    body_discovery = r'{"tenant_discovery_endpoint":"https://login.windows-ppe.net/'+ settings.TENANT_ID + '/.well-known/openid-configuration"}'
+    while total_tries > 0:
+        responses.add(responses.GET, end_point,
+                  body=body_discovery,
+                  status=200)
+
+        responses.add(responses.POST, mock_url, body=r'{"error":"' + error_string + r'","error_description":"0","error_codes":[0],"timestamp":"0","trace_id":"0","correlation_id":"0"}'
+                  , status=error_code)
+        total_tries -= 1
+
+    responses.add(responses.GET, end_point,
+                  body=body_discovery,
+                  status=200)
+    responses.add(responses.POST, mock_url, body=last_try_body, status=last_try_status)
+    try:
+        token = auth()
+        assert isinstance(token, DataLakeCredential)
+        assert not is_exception_expected
+    except (HTTPError, adal.adal_error.AdalError):
+        assert is_exception_expected
+
+
+def test_retry_auth_401():
+    __test_retry_auth(error_code=401, error_string=r'invalid_client', is_exception_expected=True)
+
+
+def test_retry_auth_104():
+    __test_retry_auth(error_code=104, error_string=r'Connection Error', is_exception_expected=False, )
+    __test_retry_auth(error_code=104, error_string=r'Connection Error', is_exception_expected=True, total_tries=6)
+
+
+def test_retry_auth_429():
+    __test_retry_auth(error_code=429, error_string=r'Too many requests', is_exception_expected=False)
+    __test_retry_auth(error_code=429, error_string=r'Too many requests', is_exception_expected=True, total_tries=6)
