b8b6178d1e00a02dfface090a692a052b7f94dfb
Akshat Harit <38331238+akharit@users.noreply.github.com>
2018-10-29 16:34:24 -0700

    Removed MSCONCAT retry changes

--- azure/datalake/store/core.py
+++ azure/datalake/store/core.py
@@ -548,8 +548,7 @@ class AzureDLFileSystem(object):
         self.azure.call('MSCONCAT', outfile.as_posix(),
                         data=bytearray(json.dumps(sources,separators=(',', ':')), encoding="utf-8"),
                         deleteSourceDirectory=delete,
-                        headers={'Content-Type': "application/json"},
-                        retry_policy=ExponentialRetryPolicy(max_retries=0))
+                        headers={'Content-Type': "application/json"})
         self.invalidate_cache(outfile)
 
     merge = concat
