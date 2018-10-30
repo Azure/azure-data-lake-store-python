2641a9a656413d04938f5ba99f5de8490fc2b14b
Akshat Harit <38331238+akharit@users.noreply.github.com>
2018-10-29 18:34:41 -0700

    Updated history.rst and version number

--- azure/datalake/store/__init__.py
+++ azure/datalake/store/__init__.py
@@ -6,7 +6,7 @@
 # license information.
 # --------------------------------------------------------------------------
 
-__version__ = "0.0.34"
+__version__ = "0.0.35"
 
 
 from .core import AzureDLFileSystem
