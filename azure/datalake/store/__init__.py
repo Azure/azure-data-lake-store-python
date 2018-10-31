a3bc5c76d285f06de62b1517730ccc66437c165f
Akshat Harit <38331238+akharit@users.noreply.github.com>
2018-10-31 16:55:29 -0700

    Fix typo

--- azure/datalake/store/__init__.py
+++ azure/datalake/store/__init__.py
@@ -6,7 +6,7 @@
 # license information.
 # --------------------------------------------------------------------------
 
-__version__ = "0.0.35"
+__version__ = "0.0.36"
 
 
 from .core import AzureDLFileSystem
