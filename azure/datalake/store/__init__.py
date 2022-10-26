3a6b2876e61c68d73bd4175d6628d818706c37e1
Tzu-ping Chung <uranusjr@gmail.com>
2022-10-26 13:07:28 +0800

    Bump to 0.0.53

--- azure/datalake/store/__init__.py
+++ azure/datalake/store/__init__.py
@@ -6,7 +6,7 @@
 # license information.
 # --------------------------------------------------------------------------
 
-__version__ = "0.0.52"
+__version__ = "0.0.53"
 
 from .core import AzureDLFileSystem
 from .multithread import ADLDownloader
