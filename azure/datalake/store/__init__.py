0fc747c951f5788ffc33c8657aef3d3b3191b372
Akshat Harit <akharit@microsoft.com>
2025-06-03 13:53:45 -0700

    Update version in _init_ to preparet for releae 1.0.1

--- azure/datalake/store/__init__.py
+++ azure/datalake/store/__init__.py
@@ -6,7 +6,7 @@
 # license information.
 # --------------------------------------------------------------------------
 
-__version__ = "1.0.0-alpha0"
+__version__ = "1.0.1"
 
 from .core import AzureDLFileSystem
 from .multithread import ADLDownloader
